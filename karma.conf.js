ad87209cb09437d21a34705c3ad786e673b9414b
John Vilk <jvilk@cs.umass.edu>
2013-05-10 14:13:46 -0400

    Oh. I fixed the unit tests. Apparently karma ships with karma-jasmine; no need to specify it inside package.json.

--- karma.conf.js
+++ karma.conf.js
@@ -1,76 +1,70 @@
 // Karma configuration
-// Generated on Thu May 09 2013 21:07:05 GMT-0400 (EDT)
-
-module.exports = function(karma) {
-  karma.configure({
-
-    // base path, that will be used to resolve files and exclude
-    basePath: '',
+// Generated on Fri May 10 2013 14:12:27 GMT-0400 (EDT)
 
 
-    // frameworks to use
-    frameworks: ['jasmine'],
+// base path, that will be used to resolve files and exclude
+basePath = '';
 
 
-    // list of files / patterns to load in the browser
-    files: [
-      'lib/browserfs.js',
-      'test/glue.js',
-      'test/node/test-buffer*.js',
-      'test/node/test-path*.js',
-      'test/node/test-fs-append-file.js'
-    ],
+// list of files / patterns to load in the browser
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  'lib/browserfs.js',
+  'test/glue.js',
+  'test/node/test-buffer*.js',
+  'test/node/test-path*.js',
+  'test/node/test-fs-append-file.js'
+];
 
 
-    // list of files to exclude
-    exclude: [
-      
-    ],
+// list of files to exclude
+exclude = [
+  
+];
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress'],
+// test results reporter to use
+// possible values: 'dots', 'progress', 'junit'
+reporters = ['progress'];
 
 
-    // web server port
-    port: 9876,
+// web server port
+port = 9876;
 
 
-    // cli runner port
-    runnerPort: 9100,
+// cli runner port
+runnerPort = 9100;
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+// enable / disable colors in the output (reporters and logs)
+colors = true;
 
 
-    // level of logging
-    // possible values: karma.LOG_DISABLE || karma.LOG_ERROR || karma.LOG_WARN || karma.LOG_INFO || karma.LOG_DEBUG
-    logLevel: karma.LOG_INFO,
+// level of logging
+// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+logLevel = LOG_INFO;
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+// enable / disable watching file and executing tests whenever any file changes
+autoWatch = true;
 
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: ['Chrome', 'Firefox', 'Safari'],
+// Start these browsers, currently available:
+// - Chrome
+// - ChromeCanary
+// - Firefox
+// - Opera
+// - Safari (only Mac)
+// - PhantomJS
+// - IE (only Windows)
+browsers = ['Chrome', 'Firefox', 'Safari', 'Opera'];
 
 
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
+// If browser does not capture in given timeout [ms], kill it
+captureTimeout = 60000;
 
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false
-  });
-};
+// Continuous Integration mode
+// if true, it capture browsers, run tests and exit
+singleRun = false;
