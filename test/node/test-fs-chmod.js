ccf8bf25ebc5e8a11a77472ddf014f3ee8a35eb8
CJ Carey <perimosocordiae@gmail.com>
2013-05-11 23:14:06 -0400

    we don't actually have fixtures, so use the tmpDir
    (and ensure the file exists)

--- test/node/test-fs-chmod.js
+++ test/node/test-fs-chmod.js
@@ -27,23 +27,24 @@ var common = BrowserFS.common;
 
 var mode = 0777;
 
-var file1 = path.join(common.fixturesDir, 'a.js'),
-    file2 = path.join(common.fixturesDir, 'a1.js');
-
-fs.chmod(file1, mode.toString(8), function(err) {
-  if (err) {
-    console.log('XXX(chmod): '+err);
-    assert.ok(false);  // chmod shouldn't throw an error
-  } else {
-    fs.stat(file1, function(err, stats){
-      assert.equal(mode, stats.mode & 0777);
-      fs.chmod(file1, mode, function(){
-        fs.stat(file1, function(err2, stats2){
-          assert.equal(mode, stats2.mode & 0777);
+var file1 = path.join(common.tmpDir, 'a.js'),
+    file2 = path.join(common.tmpDir, 'a1.js');
+fs.open(file1, 'wx', function(){
+  fs.chmod(file1, mode.toString(8), function(err) {
+    if (err) {
+      console.log('XXX(chmod): '+err);
+      assert.ok(false);  // chmod shouldn't throw an error
+    } else {
+      fs.stat(file1, function(err, stats){
+        assert.equal(mode, stats.mode & 0777);
+        fs.chmod(file1, mode, function(){
+          fs.stat(file1, function(err2, stats2){
+            assert.equal(mode, stats2.mode & 0777);
+          });
         });
       });
-    });
-  }
+    }
+  });
 });
 
 fs.open(file2, 'a', function(err, fd) {
