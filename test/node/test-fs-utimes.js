12fbebc00bb58e5f00177ddf0331b97f6120b2fd
CJ Carey <perimosocordiae@gmail.com>
2013-07-12 00:40:09 -0400

    fixing up test fs-utimes

--- test/node/test-fs-utimes.js
+++ test/node/test-fs-utimes.js
@@ -20,8 +20,6 @@
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 window.tests.fs_utimes = function() {
-var is_windows = process.platform === 'win32';
-
 var tests_ok = 0;
 var tests_run = 0;
 var rootFS = fs.getRootFS();
@@ -39,7 +37,7 @@ function stat_resource(resource) {
 }
 
 function check_mtime(resource, mtime) {
-  var mtime = fs._toUnixTimestamp(mtime);
+  mtime = fs._toUnixTimestamp(mtime);
   var stats = stat_resource(resource);
   var real_mtime = fs._toUnixTimestamp(stats.mtime);
   // check up to single-second precision
@@ -48,6 +46,7 @@ function check_mtime(resource, mtime) {
 }
 
 function expect_errno(syscall, resource, err, errno) {
+  tests_run++;
   if (err && (err.code === errno || err.code === 'ENOSYS')) {
     tests_ok++;
   } else {
@@ -56,6 +55,7 @@ function expect_errno(syscall, resource, err, errno) {
 }
 
 function expect_ok(syscall, resource, err, atime, mtime) {
+  tests_run++;
   if (!err && check_mtime(resource, mtime) ||
       err && err.code === 'ENOSYS') {
     tests_ok++;
@@ -67,21 +67,19 @@ function expect_ok(syscall, resource, err, atime, mtime) {
 // the tests assume that __filename belongs to the user running the tests
 // this should be a fairly safe assumption; testing against a temp file
 // would be even better though (node doesn't have such functionality yet)
+var filename = path.join(common.fixturesDir, 'x.txt');
 function runTest(atime, mtime, callback) {
-
   var fd, err;
   //
   // test synchronized code paths, these functions throw on failure
   //
   function syncTests() {
-    fs.utimesSync(__filename, atime, mtime);
-    expect_ok('utimesSync', __filename, undefined, atime, mtime);
-    tests_run++;
+    fs.utimesSync(filename, atime, mtime);
+    expect_ok('utimesSync', filename, undefined, atime, mtime);
 
     // some systems don't have futimes
     // if there's an error, it should be ENOSYS
     try {
-      tests_run++;
       fs.futimesSync(fd, atime, mtime);
       expect_ok('futimesSync', fd, undefined, atime, mtime);
     } catch (ex) {
@@ -96,7 +94,6 @@ function runTest(atime, mtime, callback) {
       err = ex;
     }
     expect_errno('utimesSync', 'foobarbaz', err, 'ENOENT');
-    tests_run++;
 
     err = undefined;
     try {
@@ -105,43 +102,36 @@ function runTest(atime, mtime, callback) {
       err = ex;
     }
     expect_errno('futimesSync', -1, err, 'EBADF');
-    tests_run++;
   }
 
   //
   // test async code paths
   //
-  fs.utimes(__filename, atime, mtime, function(err) {
-    expect_ok('utimes', __filename, err, atime, mtime);
+  fs.utimes(filename, atime, mtime, function(err) {
+    expect_ok('utimes', filename, err, atime, mtime);
 
     fs.utimes('foobarbaz', atime, mtime, function(err) {
       expect_errno('utimes', 'foobarbaz', err, 'ENOENT');
 
       // don't close this fd
-      if (is_windows) {
-        fd = fs.openSync(__filename, 'r+');
-      } else {
-        fd = fs.openSync(__filename, 'r');
-      }
+      fd = fs.openSync(filename, 'r');
 
       fs.futimes(fd, atime, mtime, function(err) {
         expect_ok('futimes', fd, err, atime, mtime);
 
         fs.futimes(-1, atime, mtime, function(err) {
           expect_errno('futimes', -1, err, 'EBADF');
-          syncTests();
+          if (rootFS.supportsSynch()) {
+            syncTests();
+          }
           callback();
         });
-        tests_run++;
       });
-      tests_run++;
     });
-    tests_run++;
   });
-  tests_run++;
 }
 
-var stats = fs.statSync(__filename);
+var stats = fs.statSync(filename);
 
 runTest(new Date('1982-09-10 13:37'), new Date('1982-09-10 13:37'), function() {
   runTest(new Date(), new Date(), function() {
@@ -154,8 +144,8 @@ runTest(new Date('1982-09-10 13:37'), new Date('1982-09-10 13:37'), function() {
 });
 
 process.on('exit', function() {
-  console.log('Tests run / ok:', tests_run, '/', tests_ok);
-  assert.equal(tests_ok, tests_run);
+  assert.equal(tests_ok, tests_run,
+      tests_ok + ' OK / ' + tests_run + ' total tests.');
 });
 
 };
