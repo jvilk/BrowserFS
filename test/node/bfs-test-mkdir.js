f8edeaafd7ef5d9754e71a25adef4e2431119eb4
John Vilk <jvilk@cs.umass.edu>
2013-09-16 18:00:55 -0400

    Adding a failing test -- we create files in nonexistant directories!

---
+++ test/node/bfs-test-mkdir.js
@@ -0,0 +1,14 @@
+this.tests.bfs_mkdir = function () {
+  fs.writeFile('does/not/exist.txt', 'BFS plz don\'t create this', function(err) {
+    if (!err) throw new Error('Created a file in a nonexistant directory!');
+    fs.mkdir('does', function(err) {
+      if (err) throw err;
+      fs.mkdir('does/not', function(err) {
+        if (err) throw err;
+        fs.writeFile('does/not/exist.txt', 'Should work', function(err) {
+          if (err) throw err;
+        });
+      });
+    });
+  });
+};
