50150b54901df1d5fa429b4ab5fda1a0386b63fc
John Vilk <jvilk@cs.umass.edu>
2013-10-29 12:15:36 -0400

    Path / process comments.

--- src/core/node_process.ts
+++ src/core/node_process.ts
@@ -1,21 +1,47 @@
 import node_path = require('./node_path');
 var path = node_path.path;
 
+/**
+ * Partial implementation of Node's `process` module.
+ * We implement the portions that are relevant for the filesystem.
+ * @see http://nodejs.org/api/process.html
+ */
 export class process {
   private static startTime = Date.now();
 
   private static _cwd: string = '/';
-
+  /**
+   * Changes the current working directory.
+   *
+   * **Note**: BrowserFS does not validate that the directory actually exists.
+   *
+   * @example Usage example
+   *   console.log('Starting directory: ' + process.cwd());
+   *   process.chdir('/tmp');
+   *   console.log('New directory: ' + process.cwd());
+   * @param [String] dir The directory to change to.
+   */
   public static chdir(dir: string): void {
     this._cwd = path.resolve(dir);
   }
-
+  /**
+   * Returns the current working directory.
+   * @example Usage example
+   *   console.log('Current directory: ' + process.cwd());
+   * @return [String] The current working directory.
+   */
   public static cwd(): string {
     return this._cwd;
   }
-
+  /**
+   * Returns what platform you are running on.
+   * @return [String]
+   */
   public static platform: string = 'browser';
-
+  /**
+   * Number of seconds BrowserFS has been running.
+   * @return [Number]
+   */
   public static uptime(): number {
     return ((Date.now() - this.startTime) / 1000) | 0;
   }
