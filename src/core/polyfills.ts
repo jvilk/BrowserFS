5f48a1030b780c5fb5ad458c6d7439861e36b766
John Vilk <jvilk@cs.umass.edu>
2013-11-03 16:51:44 -0500

    IE8 passes all of our unit tests!
    I'm so happy ;_;

--- src/core/polyfills.ts
+++ src/core/polyfills.ts
@@ -139,6 +139,103 @@ if (typeof setImmediate === 'undefined') {
   }
 }
 
+// IE<9 does not define indexOf.
+// From: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function(searchElement: any, fromIndex: number = 0): number {
+    if (!this) {
+      throw new TypeError();
+    }
+
+    var length = this.length;
+    if (length === 0 || pivot >= length) {
+      return -1;
+    }
+
+    var pivot = fromIndex;
+    if (pivot < 0) {
+      pivot = length - Math.abs(pivot);
+    }
+
+    for (var i = pivot; i < length; i++) {
+      if (this[i] === searchElement) {
+        return i;
+      }
+    }
+    return -1;
+  };
+}
+
+// IE<9 does not support forEach
+// From: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function(fn: (value: string, index: number, array: string[]) => void, scope?: any) {
+        var i: number, len: number;
+        for (i = 0, len = this.length; i < len; ++i) {
+            if (i in this) {
+                fn.call(scope, this[i], i, this);
+            }
+        }
+    };
+}
+
+// IE<9 does not support map
+// From: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+if (!Array.prototype.map) {
+  Array.prototype.map = function(callback: (value: string, index: number, array: string[]) => any, thisArg?: any): any[] {
+    var T, A, k;
+    if (this == null) {
+      throw new TypeError(" this is null or not defined");
+    }
+    // 1. Let O be the result of calling ToObject passing the |this| value as the argument.
+    var O = Object(this);
+    // 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".
+    // 3. Let len be ToUint32(lenValue).
+    var len = O.length >>> 0;
+    // 4. If IsCallable(callback) is false, throw a TypeError exception.
+    // See: http://es5.github.com/#x9.11
+    if (typeof callback !== "function") {
+      throw new TypeError(callback + " is not a function");
+    }
+    // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+    if (thisArg) {
+      T = thisArg;
+    }
+    // 6. Let A be a new array created as if by the expression new Array(len) where Array is
+    // the standard built-in constructor with that name and len is the value of len.
+    A = new Array(len);
+    // 7. Let k be 0
+    k = 0;
+    // 8. Repeat, while k < len
+    while(k < len) {
+      var kValue, mappedValue;
+      // a. Let Pk be ToString(k).
+      //   This is implicit for LHS operands of the in operator
+      // b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.
+      //   This step can be combined with c
+      // c. If kPresent is true, then
+      if (k in O) {
+        // i. Let kValue be the result of calling the Get internal method of O with argument Pk.
+        kValue = O[k];
+        // ii. Let mappedValue be the result of calling the Call internal method of callback
+        // with T as the this value and argument list containing kValue, k, and O.
+        mappedValue = callback.call(T, kValue, k, O);
+        // iii. Call the DefineOwnProperty internal method of A with arguments
+        // Pk, Property Descriptor {Value: mappedValue, : true, Enumerable: true, Configurable: true},
+        // and false.
+        // In browsers that support Object.defineProperty, use the following:
+        // Object.defineProperty(A, Pk, { value: mappedValue, writable: true, enumerable: true, configurable: true });
+        // For best browser support, use the following:
+        A[k] = mappedValue;
+      }
+      // d. Increase k by 1.
+      k++;
+    }
+    // 9. return A
+    return A;
+  };
+}
+
 /**
  * IE9 and below only: Injects a VBScript function that converts the
  * 'responseBody' attribute of an XMLHttpRequest into a bytestring.
--- test/setup.js
+++ test/setup.js
@@ -10,8 +10,33 @@
   // Calleable things aren't always Functions... IE9 is dumb :(
   // http://stackoverflow.com/questions/5538972/console-log-apply-not-working-in-ie9
   if (typeof console.log == "object") {
-    // Let us use Function.apply, dangit!
-    console.log = Function.prototype.bind.call(console.log, console);
+    if (Function.prototype.bind) {
+      // Let us use Function.apply, dangit!
+      console.log = Function.prototype.bind.call(console.log, console);
+    } else {
+      // IE<9 does not define bind. :(
+      // Use a half-assed polyfill function.
+      var oglog = console.log;
+      console.log = function() {
+        switch(arguments.length) {
+          case 0:
+            return oglog();
+          case 1:
+            return oglog(arguments[0]);
+          case 2:
+            return oglog(arguments[0], arguments[1]);
+          case 3:
+            return oglog(arguments[0], arguments[1], arguments[2]);
+          case 4:
+            return oglog(arguments[0], arguments[1], arguments[2], arguments[3]);
+          case 5:
+            return oglog(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4]);
+          default:
+            oglog("WARNING: Calling console.log with > 5 arguments...");
+            return oglog(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4]);
+        }
+      };
+    }
   }
   // Add a TAP reporter so we get decent console output.
   jasmine.getEnv().addReporter(new TAPReporter(function() {
