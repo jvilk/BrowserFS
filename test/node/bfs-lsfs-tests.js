a00366384a5ad318b10dc784d3491eaee03a9689
John Vilk <jvilk@cs.umass.edu>
2013-11-26 13:15:26 -0500

    Adding a LocalStorage test that tries to fill up LocalStorage.

---
+++ test/node/bfs-lsfs-tests.js
@@ -0,0 +1,19 @@
+/**
+ * Unit tests specifically crafted for the LocalStorage file system.
+ */
+window.tests.bfs_lsfs_tests = function() {
+  if (!(fs.getRootFS() instanceof BrowserFS.FileSystem.LocalStorage)) return;
+
+  // While LocalStorage is supposed to only store 5MB, our compression enables
+  // us to store 10MB in some cases...
+  var bigbuff = new Buffer(10*1024*1024);
+  var errorThrown = false;
+  // Try to write it to local storage. Should get an error!
+  try {
+    fs.writeFileSync("/bigfile.txt", bigbuff);
+  } catch (e) {
+    errorThrown = true;
+    assert(e.code === 'ENOSPC');
+  }
+  assert(errorThrown);
+};
