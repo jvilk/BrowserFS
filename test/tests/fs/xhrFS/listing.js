45836bf9bd1c4b95d97762b5f96275c797e5be9d
hrj <harshad.rj@gmail.com>
2016-01-14 23:23:46 +0530

    test: avoid creating extra root fs

--- test/tests/fs/xhrFS/listing.js
+++ test/tests/fs/xhrFS/listing.js
@@ -1,14 +1,11 @@
 /**
- * Unit tests for MountableFileSystem's mount/unmount features.
+ * Unit tests for XHRFS
  */
 var fs = require('fs'),
     assert = require('wrapped-assert');
 
 module.exports = function() {
   var oldRootFS = fs.getRootFS();
-
-  var rootForXFS = new BrowserFS.FileSystem.InMemory();
-  BrowserFS.initialize(rootForXFS);
 
   var listing = {
     "README.md":null,
@@ -27,34 +24,34 @@ module.exports = function() {
   }
 
   var newXFS = new BrowserFS.FileSystem.XmlHttpRequest(listing, "test/tests/fs/xhrFS");
-  rootForXFS.mount("/tRoot", newXFS);
+  BrowserFS.initialize(newXFS);
+
 
   var t1text = 'Invariant fail: Can query folder that contains items and a mount point.';
   var expectedTestListing = ['README.md', 'src', 'test'];
-  var testListing = fs.readdirSync('/tRoot').sort();
+  var testListing = fs.readdirSync('/').sort();
   assert.deepEqual(testListing, expectedTestListing, t1text);
 
-  fs.readdir('/tRoot', function(err, files) {
+  fs.readdir('/', function(err, files) {
     assert(!err, t1text);
     assert.deepEqual(files.sort(), expectedTestListing, t1text);
-    fs.stat("/tRoot/test/fixtures/static/49chars.txt", function(err, stats) {
+    fs.stat("/test/fixtures/static/49chars.txt", function(err, stats) {
       assert(!err, "Can stat an existing file");
       assert(stats.isFile(), "File should be interpreted as a file");
       assert(!stats.isDirectory(), "File should be interpreted as a directory");
       assert(stats.size == 49, "file size should match");
     });
 
-    fs.stat("/tRoot/src/backend", function(err, stats) {
+    fs.stat("/src/backend", function(err, stats) {
       assert(!err, "Can stat an existing directory");
       assert(stats.isDirectory(), "directory should be interpreted as a directory");
       assert(!stats.isFile(), "directory should be interpreted as a file");
     });
 
-    fs.stat("/tRoot/src/not-existing-name", function(err, stats) {
+    fs.stat("/src/not-existing-name", function(err, stats) {
       assert(!!err, "Non existing file should return an error");
     });
 
-    rootForXFS.umount("/tRoot");
   });
 
 
