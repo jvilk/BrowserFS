473b075c9b613d355ab31129e10ec3207ab115ec
Bobby Powers <bobbypowers@gmail.com>
2016-07-12 09:23:00 -0400

    OverlayFS: protect internal OverlayFS with LockedFS

--- src/backend/OverlayFS.ts
+++ src/backend/OverlayFS.ts
@@ -4,7 +4,8 @@ import {FileFlag, ActionType} from '../core/file_flag';
 import util = require('../core/util');
 import file = require('../core/file');
 import Stats from '../core/node_fs_stats';
-import preload_file = require('../generic/preload_file');
+import {PreloadFile} from '../generic/preload_file';
+import {LockedFS} from '../generic/locked_fs';
 import path = require('path');
 let deletionLogPath = '/.deletedFiles.log';
 
@@ -22,8 +23,8 @@ function getFlag(f: string): FileFlag {
 /**
  * Overlays a RO file to make it writable.
  */
-class OverlayFile extends preload_file.PreloadFile<OverlayFS> implements file.File {
-  constructor(fs: OverlayFS, path: string, flag: FileFlag, stats: Stats, data: Buffer) {
+class OverlayFile extends PreloadFile<UnlockedOverlayFS> implements file.File {
+  constructor(fs: UnlockedOverlayFS, path: string, flag: FileFlag, stats: Stats, data: Buffer) {
     super(fs, path, flag, stats, data);
   }
 
@@ -60,7 +61,7 @@ class OverlayFile extends preload_file.PreloadFile<OverlayFS> implements file.Fi
  * writable file system. Deletes are persisted via metadata stored on the writable
  * file system.
  */
-export default class OverlayFS extends file_system.SynchronousFileSystem implements file_system.FileSystem {
+export class UnlockedOverlayFS extends file_system.SynchronousFileSystem implements file_system.FileSystem {
   private _writable: file_system.FileSystem;
   private _readable: file_system.FileSystem;
   private _isInitialized: boolean = false;
@@ -145,13 +146,13 @@ export default class OverlayFS extends file_system.SynchronousFileSystem impleme
     return true;
   }
 
-  public _syncAsync(file: preload_file.PreloadFile<OverlayFS>, cb: (err: ApiError)=>void): void {
+  public _syncAsync(file: PreloadFile<UnlockedOverlayFS>, cb: (err: ApiError)=>void): void {
     this.createParentDirectoriesAsync(file.getPath(), () => {
       this._writable.writeFile(file.getPath(), file.getBuffer(), null, getFlag('w'), file.getStats().mode, cb);
     });
   }
 
-  public _syncSync(file: preload_file.PreloadFile<OverlayFS>): void {
+  public _syncSync(file: PreloadFile<UnlockedOverlayFS>): void {
     this.createParentDirectories(file.getPath());
     this._writable.writeFileSync(file.getPath(), file.getBuffer(), null, getFlag('w'), file.getStats().mode);
   }
@@ -446,4 +447,22 @@ export default class OverlayFS extends file_system.SynchronousFileSystem impleme
         getFlag('w'), this.statSync(p, false).mode);
     }
   }
+}
+
+export default class OverlayFS extends LockedFS<UnlockedOverlayFS> {
+	constructor(writable: file_system.FileSystem, readable: file_system.FileSystem) {
+		super(UnlockedOverlayFS, writable, readable);
+	}
+
+	initialize(cb: (err?: ApiError) => void): void {
+		super.initialize(cb);
+	}
+
+	static isAvailable(): boolean {
+		return UnlockedOverlayFS.isAvailable();
+	}
+
+	getOverlayedFileSystems(): { readable: file_system.FileSystem; writable: file_system.FileSystem; } {
+		return super.getFSUnlocked().getOverlayedFileSystems();
+	}
 }
