f837c7d9615860f816757393536a7296dfcffc47
John Vilk <jvilk@cs.umass.edu>
2017-08-07 14:00:22 -0700

    Fix strict mode error in Firefox.

--- test/harness/factories/dbfs_factory.ts
+++ test/harness/factories/dbfs_factory.ts
@@ -5,6 +5,35 @@ import {ApiError} from '../../../src/core/api_error';
 
 declare const Dropbox: typeof DropboxTypes.Dropbox;
 export default function DBFSFactory(cb: (name: string, obj: FileSystem[]) => void): void {
+  function login(creds: DropboxTypes.DropboxOptions) {
+    const client = new Dropbox(creds);
+    client.usersGetCurrentAccount(undefined).then((res) => {
+      return new Promise<DropboxFileSystem>((resolve, reject) => {
+        console.debug(`Successfully connected to ${res.name.display_name}'s Dropbox.`);
+        DropboxFileSystem.Create({ client }, (e, fs?) => {
+          if (fs) {
+            fs.empty((e) => {
+              if (e) {
+                reject(e);
+              } else {
+                resolve(fs);
+              }
+            });
+          } else {
+            reject(e);
+          }
+        });
+      });
+    }).then((fs) => {
+      cb("Dropbox", [fs]);
+    }).catch((e: DropboxTypes.Error<any> | ApiError) => {
+      if (e instanceof ApiError) {
+        throw e;
+      } else {
+        throw new Error(`Failed to log in to Dropbox: ${e.user_message.text}`);
+      }
+    });
+  }
   if (DropboxFileSystem.isAvailable()) {
     // Authenticate with pregenerated unit testing credentials.
     const req = new XMLHttpRequest();
@@ -18,36 +47,6 @@ export default function DBFSFactory(cb: (name: string, obj: FileSystem[]) => voi
       login(JSON.parse(req.response));
     };
     req.send();
-
-    function login(creds: DropboxTypes.DropboxOptions) {
-      const client = new Dropbox(creds);
-      client.usersGetCurrentAccount(undefined).then((res) => {
-        return new Promise<DropboxFileSystem>((resolve, reject) => {
-          console.debug(`Successfully connected to ${res.name.display_name}'s Dropbox.`);
-          DropboxFileSystem.Create({ client }, (e, fs?) => {
-            if (fs) {
-              fs.empty((e) => {
-                if (e) {
-                  reject(e);
-                } else {
-                  resolve(fs);
-                }
-              });
-            } else {
-              reject(e);
-            }
-          });
-        });
-      }).then((fs) => {
-        cb("Dropbox", [fs]);
-      }).catch((e: DropboxTypes.Error<any> | ApiError) => {
-        if (e instanceof ApiError) {
-          throw e;
-        } else {
-          throw new Error(`Failed to log in to Dropbox: ${e.user_message.text}`);
-        }
-      });
-    }
   } else {
     cb('Dropbox', []);
   }
