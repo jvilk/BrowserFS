c047e08d81dc48766d95e75bb2588dc8df6605e1
Will Hilton <wmhilton@gmail.com>
2017-08-26 23:27:51 -0400

    Switch from using Create to getFileSystem

--- test/harness/factories/middleware_factory.ts
+++ test/harness/factories/middleware_factory.ts
@@ -1,20 +1,20 @@
 import {FileSystem} from '../../../src/core/file_system';
 import InMemory from '../../../src/backend/InMemory';
 import Middleware from '../../../src/backend/Middleware';
+import {getFileSystem} from '../../../src/core/browserfs';
 
-export default function HTTPDownloadFSFactory(cb: (name: string, objs: FileSystem[]) => void): void {
+export default function MiddlewareFactory(cb: (name: string, objs: FileSystem[]) => void): void {
   if (InMemory.isAvailable() && Middleware.isAvailable()) {
-    InMemory.Create({}, (e, mfs) => {
+    getFileSystem({
+      fs: "Middleware",
+      options: {
+        wrapped: { fs: 'InMemory' }
+      }
+    }, (e, fs?) => {
       if (e) {
         throw e;
       } else {
-        Middleware.Create({ wrapped: mfs }, (e, fs) => {
-          if (e) {
-            throw e;
-          } else {
-            cb('Middleware', [fs]);
-          }
-        });
+        cb('Middleware', [fs]);
       }
     });
   } else {
