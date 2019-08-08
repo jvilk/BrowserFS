3d61e7631653369c1ca554ad51a1288887d1a4f6
dsl400 <dsl400@gmail.com>
2019-08-08 22:38:23 +0300

    Update node_fs_stats.ts
     changing implements to extends

--- src/core/node_fs_stats.ts
+++ src/core/node_fs_stats.ts
@@ -16,7 +16,7 @@ export enum FileType {
  * @see http://nodejs.org/api/fs.html#fs_class_fs_stats
  * @see http://man7.org/linux/man-pages/man2/stat.2.html
  */
-export default class Stats implements fs.Stats {
+export default class Stats Extends fs.Stats {
   public static fromBuffer(buffer: Buffer): Stats {
     const size = buffer.readUInt32LE(0),
       mode = buffer.readUInt32LE(4),
