2ca14262d835b1d3566e87e23ddbdbbf02bf53fe
NexusNull <p.wellershaus@googlemail.com>
2020-12-21 15:02:39 +0100

    remove unnecessary fs-extra

--- scripts/webdav_server.ts
+++ scripts/webdav_server.ts
@@ -1,26 +1,26 @@
 const webdav = require('webdav-server').v2;
 const express = require('express');
-const fs = require('fs-extra');
+const fs = require('fs');
 const os = require('os');
 const path = require('path');
 
 const directory = path.join(os.tmpdir(), Math.random().toString(16).substr(2, 8));
 
-fs.mkdir(directory);
+fs.mkdir(directory, () => {
+  const server = new webdav.WebDAVServer({
+    rootFileSystem: new webdav.PhysicalFileSystem(directory)
+  });
 
-const server = new webdav.WebDAVServer({
-  rootFileSystem: new webdav.PhysicalFileSystem(directory)
+  const app = express();
+
+  app.use(function (req: any, res: any, next: () => void) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+  });
+
+  app.use(express.static("./"));
+  app.listen(180);
+  app.use(webdav.extensions.express('/fs', server));
 });
-
-const app = express();
-
-app.use(function (req: any, res: any, next: () => void) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
-});
-
-app.use(express.static("./"));
-app.listen(180);
-app.use(webdav.extensions.express('/fs', server));
